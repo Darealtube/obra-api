f986a18363c9e79b5d26f64f6916a0faa4037656
Darealtube <evajavier143@gmail.com>
2021-05-21 14:57:35 +0800

    Test Update

--- next.config.js
+++ next.config.js
@@ -6,10 +6,13 @@ module.exports = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://obra-website.vercel.app http://localhost:3000",
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Headers",
