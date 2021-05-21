c52d1766b73efb3aa0546ca1973943aaa1474d74
Darealtube <evajavier143@gmail.com>
2021-05-21 15:05:04 +0800

    Test Update

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://obra-website.vercel.app http://localhost:3000",
+            value: "https://obra-website.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
