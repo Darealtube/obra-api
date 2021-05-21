66bd6b4cb91423c447f41baf303cb9682db7f2e5
Darealtube <evajavier143@gmail.com>
2021-05-21 15:37:47 +0800

    Test Update

--- next.config.js
+++ next.config.js
@@ -16,7 +16,8 @@ module.exports = {
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "*",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
           },
         ],
       },
