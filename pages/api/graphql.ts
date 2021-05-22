cc93115e86cec07fd4e44dab3e111a7ca79b509e
Darealtube <evajavier143@gmail.com>
2021-05-22 13:09:06 +0800

    Test Update

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -33,7 +33,7 @@ const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://obra-website.vercel.app"
+    "https://obra-website.vercel.app http://localhost:3000"
   );
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
